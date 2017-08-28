ddd62e416f955ef069b8517ee525a3ac8b839989
Millicent Jepkoech <mjepkoech@mjepkoech.local>
2017-08-28 17:42:04 +0300

    Initial Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ticketing
+                    Newest!!!!
                 </div>
 
                 <div class="links">
